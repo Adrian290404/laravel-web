a52068edf8ff109fdb988350d83e3b7aab41f8ca
Adrian290404 <adrianmg2004.29@gmail.com>
2025-02-19 08:17:33 +0100

    routes and controllers created

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ActivityController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -24,5 +20,12 @@ Route::get('/activities', [ActivityController::class, 'index'])->name('activitie
 Route::get('/activities/{id}', [ActivityController::class, 'show'])->name('activities.show');
 Route::get('/activities/create', [ActivityController::class, 'create'])->name('activities.create');
 Route::get('/activities/edit/{id}', [ActivityController::class, 'edit'])->name('activities.edit');
+
+Route::get('/', function () { return view('hotel.home');});
+Route::get('/about', function () { return view('hotel.about'); })->name('about');
+Route::get('/contact', function () { return view('hotel.contact'); })->name('about');
+Route::get('/offers', function () { return view('hotel.offers');})->name('offers');
+Route::get('/rooms', [RoomController::class, 'index'])->name('rooms');
+Route::get('/roomDetails', [RoomController::class, 'show'])->name('roomDetails');
 
 require __DIR__.'/auth.php';
