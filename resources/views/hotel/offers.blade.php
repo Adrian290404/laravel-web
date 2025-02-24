2406557f943310496aa292b11322a558c69b7232
Adrian290404 <adrianmg2004.29@gmail.com>
2025-02-24 08:50:18 +0100

    offers fixed

--- resources/views/hotel/offers.blade.php
+++ resources/views/hotel/offers.blade.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Offers</title>
-    <link rel="stylesheet" href="{{ asset('css/hotel/offers.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/hotel/header.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/hotel/footer.css') }}">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
-    <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
-    <script src="{{ asset('js/hotel/offers/rooms.js') }}" defer></script>
-    <script src="{{ asset('js/hotel/header/dropdown.js') }}" defer></script>
-</head>
-<body>
 @extends('layouts.hotel')
 
 @section('title', 'Offers')
