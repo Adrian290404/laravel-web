65116b296ea42b7cb95cec8c745b3a445961fd72
Adrian290404 <adrianmg2004.29@gmail.com>
2025-02-24 09:20:06 +0100

    toasts on contact form

--- resources/views/layouts/hotel.blade.php
+++ resources/views/layouts/hotel.blade.php
@@ -10,6 +10,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     @yield('head')
+    <!-- Toastr CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" />
     <script src="{{ asset('js/hotel/header/dropdown.js') }}" defer></script>
 </head>
 <body>
@@ -20,13 +22,25 @@
         <nav class="container__card">
             <img class="container__card__dropdown" id="dropdownOpen" src="{{ asset('img/hotel/header/icons/dropdown.png') }}">
             <img class="container__card__dropdown hidden" id="dropdownClose" src="{{ asset('img/hotel/header/icons/closeDropdown.png') }}">
-            <a class="container__card__link" href="{{ route('home') }}"><img class="container__card__link__logo container__card__link__logo--mobile" src="{{ asset('img/hotel/header/logo.png') }}"></a>
-            <a class="container__card__link" href="{{ route('home') }}"><img class="container__card__link__logo container__card__link__logo--desktop" src="{{ asset('img/hotel/header/logoDesktop.png') }}"></a>
+            <a class="container__card__link" href="{{ route('home') }}">
+                <img class="container__card__link__logo container__card__link__logo--mobile" src="{{ asset('img/hotel/header/logo.png') }}">
+            </a>
+            <a class="container__card__link" href="{{ route('home') }}">
+                <img class="container__card__link__logo container__card__link__logo--desktop" src="{{ asset('img/hotel/header/logoDesktop.png') }}">
+            </a>
             <ul class="container__card__navbar" id="navBar">
-                <a class="container__card__navbar__link" href="{{ route('about') }}"><li class="container__card__navbar__link__page">About Us</li></a>
-                <a class="container__card__navbar__link" href="{{ route('rooms') }}"><li class="container__card__navbar__link__page">Rooms</li></a>
-                <a class="container__card__navbar__link" href="{{ route('offers') }}"><li class="container__card__navbar__link__page">Offers</li></a>
-                <a class="container__card__navbar__link" href="{{ route('contact') }}"><li class="container__card__navbar__link__page">Contact</li></a>
+                <a class="container__card__navbar__link" href="{{ route('about') }}">
+                    <li class="container__card__navbar__link__page">About Us</li>
+                </a>
+                <a class="container__card__navbar__link" href="{{ route('rooms') }}">
+                    <li class="container__card__navbar__link__page">Rooms</li>
+                </a>
+                <a class="container__card__navbar__link" href="{{ route('offers') }}">
+                    <li class="container__card__navbar__link__page">Offers</li>
+                </a>
+                <a class="container__card__navbar__link" href="{{ route('contact') }}">
+                    <li class="container__card__navbar__link__page">Contact</li>
+                </a>
             </ul>
             <div class="container__card__icons">
                 <img class="container__card__icons__icon" src="{{ asset('img/hotel/header/icons/login.png') }}">
@@ -41,7 +55,9 @@
         <section class="footer__content">
             <article class="footer__content__introduction">
                 <img class="footer__content__introduction__logo" src="{{ asset('img/hotel/footer/logo.png') }}">
-                <p class="footer__content__introduction__text">Lorem ipsum dolor sit amet, consect etur adipisicing elit, sed doing eius mod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitat ion ullamco laboris nisi.</p>
+                <p class="footer__content__introduction__text">
+                    Lorem ipsum dolor sit amet, consect etur adipisicing elit, sed doing eius mod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitat ion ullamco laboris nisi.
+                </p>
                 <div class="footer__content__introduction__social-media">
                     <img class="footer__content__introduction__social-media__icon" src="{{ asset('img/hotel/footer/icons/facebook.png') }}">
                     <img class="footer__content__introduction__social-media__icon footer__content__introduction__social-media__icon--active" src="{{ asset('img/hotel/footer/icons/twitter.png') }}">
@@ -99,5 +115,24 @@
             <h2 class="footer__terms-of-use__text">Terms of use | Privacy Environmental Policy</h2>
         </section>
     </footer>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script>
+        @if(session('success'))
+            toastr.success("{{ session('success') }}");
+        @endif
+
+        @if(session('error'))
+            toastr.error("{{ session('error') }}");
+        @endif
+
+        @if($errors->any())
+            @foreach($errors->all() as $error)
+                toastr.error("{{ $error }}");
+            @endforeach
+        @endif
+    </script>
+    @yield('scripts')
 </body>
 </html>
