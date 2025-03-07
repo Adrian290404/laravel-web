59e1f7231d69dbdfbdc8a64340c32ac22c93e480
Adrian290404 <adrianmg2004.29@gmail.com>
2025-03-07 09:54:05 +0100

    changes on home

--- public/js/hotel/home/formValidation.js
+++ public/js/hotel/home/formValidation.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const dateStart = document.getElementById('date-start');
-    const dateEnd   = document.getElementById('date-end');
+    const dateEnd = document.getElementById('date-end');
 
     function getToday() {
         let today = new Date();
-        let year  = today.getFullYear();
+        let year = today.getFullYear();
         let month = (today.getMonth() + 1).toString().padStart(2, '0');
-        let day   = today.getDate().toString().padStart(2, '0');
+        let day = today.getDate().toString().padStart(2, '0');
         return `${year}-${month}-${day}`;
     }
 
     function getTomorrow() {
         let tomorrow = new Date();
         tomorrow.setDate(tomorrow.getDate() + 1);
-        let year  = tomorrow.getFullYear();
+        let year = tomorrow.getFullYear();
         let month = (tomorrow.getMonth() + 1).toString().padStart(2, '0');
-        let day   = tomorrow.getDate().toString().padStart(2, '0');
+        let day = tomorrow.getDate().toString().padStart(2, '0');
         return `${year}-${month}-${day}`;
     }
 
@@ -27,8 +27,8 @@ document.addEventListener("DOMContentLoaded", function() {
             let selectedDate = new Date(dateStart.value);
             selectedDate.setDate(selectedDate.getDate() + 1);
             let month = (selectedDate.getMonth() + 1).toString().padStart(2, '0');
-            let day   = selectedDate.getDate().toString().padStart(2, '0');
-            let year  = selectedDate.getFullYear();
+            let day = selectedDate.getDate().toString().padStart(2, '0');
+            let year = selectedDate.getFullYear();
             dateEnd.min = `${year}-${month}-${day}`;
         }
     });
@@ -39,5 +39,17 @@ document.addEventListener("DOMContentLoaded", function() {
                 event.preventDefault(); 
             }
         }
+    });
+
+    document.querySelectorAll('.home__calendar__form__container__input-container__icon').forEach(function(icon) {
+        icon.addEventListener('click', function() {
+            let input = icon.nextElementSibling;
+            if (typeof input.showPicker === "function") {
+                input.showPicker();
+            } else {
+                input.focus();
+                input.click();
+            }
+        });
     });
 });
