0e09bec14505bc57a4ebe60ceef8591526524a9c
Adrian290404 <adrianmg2004.29@gmail.com>
2025-03-07 08:40:57 +0100

    changes on web.php

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('activities', ActivityController::class);
-
 Route::get('/activities', [ActivityController::class, 'index'])->name('activities.index');
 Route::get('/activities/{id}', [ActivityController::class, 'show'])->name('activities.show');
 Route::get('/activities/create', [ActivityController::class, 'create'])->name('activities.create');
